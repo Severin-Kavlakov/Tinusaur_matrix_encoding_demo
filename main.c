76806e7116c0b3b583fea8ea92027340dbf70afd
MoonLordTheSecond <moonlordthesecond@abv.bg>
2026-04-23 15:43:07 +0300

    0.03_working

--- main.c
+++ main.c
@@ -9,34 +9,35 @@ void halt_program() {
 }
 
 //-VARIABLES---------------------------------------------------------------------------------
-char message[]   = {"HELLO WORLD"};
+char message[]   = {"IX A"};
 char symbols[]   = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
 					'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
 					'U', 'V', 'W', 'X', 'Y', 'Z', ' '				 };
 
-#define totalChars 11 //changeable BUT must be accurate for this program to run
-int realTotalChars = sizeof(message)-1; // 11, minus 1 because of '/0' aka NULL TERMINATOR char, automatically added at the end
+#define totalChars 4 //changeable BUT must be accurate for program to run
+int realTotalChars = sizeof(message)-1;
+// 11, minus 1 because of '/0' aka NULL TERMINATOR char, automatically added at the end
 
-#define totalSymbols 27 //constant
+int totalSymbols = 27; //constant
 
 int encodingValues[totalSymbols][3] = {
-        {1, 1, 1}, {1, 1, 2}, {1, 1, 3},
-        {1, 2, 1}, {1, 2, 2}, {1, 2, 3},
-        {1, 3, 1}, {1, 3, 2}, {1, 3, 3},
-        {2, 1, 1}, {2, 1, 2}, {2, 1, 3},
-        {2, 2, 1}, {2, 2, 2}, {2, 2, 3},
-        {2, 3, 1}, {2, 3, 2}, {2, 3, 3},
-        {3, 1, 1}, {3, 1, 2}, {3, 1, 3},
-        {3, 2, 1}, {3, 2, 2}, {3, 2, 3},
-        {3, 3, 1}, {3, 3, 2}, {3, 3, 3}
+    {1, 1, 1}, {1, 1, 2}, {1, 1, 3},
+    {1, 2, 1}, {1, 2, 2}, {1, 2, 3},
+    {1, 3, 1}, {1, 3, 2}, {1, 3, 3},
+    {2, 1, 1}, {2, 1, 2}, {2, 1, 3},
+    {2, 2, 1}, {2, 2, 2}, {2, 2, 3},
+    {2, 3, 1}, {2, 3, 2}, {2, 3, 3},
+    {3, 1, 1}, {3, 1, 2}, {3, 1, 3},
+    {3, 2, 1}, {3, 2, 2}, {3, 2, 3},
+    {3, 3, 1}, {3, 3, 2}, {3, 3, 3}
 };
 
 int encodedAndEncryptedChars[totalChars][3];
 
-int encriptionKeyMatrix[3][3] = { 
-	{1, 1, 7},
-	{0, 1, 2},
-	{0, 0, 1}
+int encryptionKeyMatrix[3][3] = { 
+	{1, 2, 5},
+	{2, 5, 7},
+	{1, 2, 4}
 };
 
 //-FUNCTIONS---------------------------------------------------------------------------------
@@ -59,65 +60,73 @@ void check_message_compatability(char message[]) {
         }
     }
     if (counter != totalChars) {  //STOP if incompatible chars found
-		halt_program();
+        halt_program();
     }
 }
 
-void encode_char(char myChar, int encodedChar[3]) {
+void encode_and_encrypt_char(char myChar, int out[3]) {
+    //encode
+    int encoded[3];
 	for (int i = 0; i <= totalSymbols-1; i++) {
 		if (myChar == symbols[i]) {
-			encodedChar[0] = encodingValues[i][0];
-			encodedChar[1] = encodingValues[i][1];
-			encodedChar[2] = encodingValues[i][2];
+			encoded[0] = encodingValues[i][0];
+			encoded[1] = encodingValues[i][1];
+			encoded[2] = encodingValues[i][2];
+			break;
 		}
 	}
-}
-
-void encrypt_char(char myChar) {
 	
+    //encrypt
+    int temp[3] = {0, 0, 0}; 
+    for (int row = 0; row <= 2; row++) {
+        for (int col = 0; col <= 2; col++) {
+            temp[row] += encryptionKeyMatrix[row][col] * encoded[col];
+        }
+    }
+    for (int i = 0; i <= 2; i++) {
+        int mod = temp[i] % 3;
+        if (mod < 0) {mod += 3;} // account for negatives
+        out[i] = mod + 1;        // from 0..2 to 1..3
+    }
 }
-
-
-
 
 void out_encrypted_char(int encoded[3]) { 
 	for (int red = 0; red < encoded[0]; red++) { // RED LED HIGH ; LOW
-	  PORTB |= (1 << PB0);  _delay_ms(250);
-	  PORTB &= ~(1 << PB0); _delay_ms(250);
+	  PORTB |= (1 << PB0);  _delay_ms(500);
+	  PORTB &= ~(1 << PB0); _delay_ms(500);
 	}
 	
 	for (int yellow = 0; yellow < encoded[1]; yellow++) { // YELLOW LED HIGH ; LOW
-	  PORTB |= (1 << PB1);  _delay_ms(250);
-	  PORTB &= ~(1 << PB1); _delay_ms(250);
+	  PORTB |= (1 << PB1);  _delay_ms(500);
+	  PORTB &= ~(1 << PB1); _delay_ms(500);
 	}
 	
 	for (int green = 0; green < encoded[2]; green++) { // GREEN LED HIGH ; LOW
-	  PORTB |= (1 << PB2);  _delay_ms(250);
-	  PORTB &= ~(1 << PB2); _delay_ms(250);
+	  PORTB |= (1 << PB2);  _delay_ms(500);
+	  PORTB &= ~(1 << PB2); _delay_ms(500);
 	}
 }
 
 
-
 int main(void) {
-    DDRB |= (1 << PB0);	// LED: Set port as output
-    DDRB |= (1 << PB1);	// LED: Set port as output
-    DDRB |= (1 << PB2);	// LED: Set port as output
-
-	check_total_chars_count();
+    DDRB |= (1 << PB0);	// LED: port as output
+    DDRB |= (1 << PB1);	// LED:  port as output
+    DDRB |= (1 << PB2);	// LED: port as output
+    
+    check_total_chars_count();
     check_message_compatability(message);
-
+    
 	for(int i = 0; i <= totalChars-1; i++) {
-		encode_char(message[i], encodedAndEncryptedChars[i]);
+		encode_and_encrypt_char(message[i], encodedAndEncryptedChars[i]);
 	}
 
-
-
-
 	while(1) {
+	    _delay_ms(3000);
+	    
 		for (int i = 0; i <= totalChars-1; i++) { // for every encoded char in encodedChars: out to leds
 			out_encrypted_char(encodedAndEncryptedChars[i]);  // access sub array located at [i] from a max of [totalChars=11]
 		}
+		
 	    _delay_ms(10000); // loop
 	}
 	
