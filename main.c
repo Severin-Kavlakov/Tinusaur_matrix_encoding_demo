f2d0e511827d1d15ec4d64c27d0bf199f4769030
MoonLordTheSecond <moonlordthesecond@abv.bg>
2026-04-25 11:52:45 +0300

    0.04_feat: halt() and char sizes
    NEW COMMIT STYLE:
    0.00_feat/fix/style/refactor: message

--- main.c
+++ main.c
@@ -1,24 +1,15 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-void halt_program() {
-	PORTB |= (1 << PB0); //RED LED: HIGH
-	PORTB |= (1 << PB1); //YELLOW LED: HIGH
-	PORTB |= (1 << PB2); //GREEN LED: HIGH
-	while(1);
-}
 
 //-VARIABLES---------------------------------------------------------------------------------
-char message[]   = {"IX A"};
-char symbols[]   = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
-					'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
-					'U', 'V', 'W', 'X', 'Y', 'Z', ' '				 };
+char message[]   = { "IX A" };
+char symbols[27] = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
+					 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 
+					 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', ' ' };
 
-#define totalChars 4 //changeable BUT must be accurate for program to run
-int realTotalChars = sizeof(message)-1;
-// 11, minus 1 because of '/0' aka NULL TERMINATOR char, automatically added at the end
-
-int totalSymbols = 27; //constant
+#define totalChars   sizeof(message)-1 // #defined to be known at compile time ; to allow Variable Length Arrays
+#define totalSymbols sizeof(symbols)   // explicit array size
 
 int encodingValues[totalSymbols][3] = {
     {1, 1, 1}, {1, 1, 2}, {1, 1, 3},
@@ -40,17 +31,24 @@ int encryptionKeyMatrix[3][3] = {
 	{1, 2, 4}
 };
 
+
 //-FUNCTIONS---------------------------------------------------------------------------------
 
-void check_total_chars_count() {
-	if (totalChars != realTotalChars) {
-		halt_program();
+void halt_program(void) {
+	while(1) {
+		PORTB |= (1 << PB0); //RED LED: HIGH
+		PORTB |= (1 << PB1); //YELLOW LED: HIGH
+		PORTB |= (1 << PB2); //GREEN LED: HIGH
+		_delay_ms(100);
+		PORTB &= ~(1 << PB0);
+		PORTB &= ~(1 << PB1);
+		PORTB &= ~(1 << PB2);
+		_delay_ms(100);
 	}
 }
 
-void check_message_compatability(char message[]) {
+int check_message_compatability(char message[]) {
     int counter=0;
-    
     for (int i=0; i <= totalChars-1; i++) { //check for incompatible chars
         for (int j=0; j <= totalSymbols-1; j++) {
             if (message[i] == symbols[j]) {
@@ -59,9 +57,7 @@ void check_message_compatability(char message[]) {
             }
         }
     }
-    if (counter != totalChars) {  //STOP if incompatible chars found
-        halt_program();
-    }
+    return (counter == totalChars) ? 1 : 0; //return 0 if incompatible chars found
 }
 
 void encode_and_encrypt_char(char myChar, int out[3]) {
@@ -75,7 +71,7 @@ void encode_and_encrypt_char(char myChar, int out[3]) {
 			break;
 		}
 	}
-	
+
     //encrypt
     int temp[3] = {0, 0, 0}; 
     for (int row = 0; row <= 2; row++) {
@@ -95,12 +91,12 @@ void out_encrypted_char(int encoded[3]) {
 	  PORTB |= (1 << PB0);  _delay_ms(500);
 	  PORTB &= ~(1 << PB0); _delay_ms(500);
 	}
-	
+
 	for (int yellow = 0; yellow < encoded[1]; yellow++) { // YELLOW LED HIGH ; LOW
 	  PORTB |= (1 << PB1);  _delay_ms(500);
 	  PORTB &= ~(1 << PB1); _delay_ms(500);
 	}
-	
+
 	for (int green = 0; green < encoded[2]; green++) { // GREEN LED HIGH ; LOW
 	  PORTB |= (1 << PB2);  _delay_ms(500);
 	  PORTB &= ~(1 << PB2); _delay_ms(500);
@@ -110,25 +106,27 @@ void out_encrypted_char(int encoded[3]) {
 
 int main(void) {
     DDRB |= (1 << PB0);	// LED: port as output
-    DDRB |= (1 << PB1);	// LED:  port as output
+    DDRB |= (1 << PB1);	// LED: port as output
     DDRB |= (1 << PB2);	// LED: port as output
-    
-    check_total_chars_count();
-    check_message_compatability(message);
-    
+
+    if(check_message_compatability(message)!=1) {
+		halt_program();
+	}
+
 	for(int i = 0; i <= totalChars-1; i++) {
 		encode_and_encrypt_char(message[i], encodedAndEncryptedChars[i]);
 	}
 
 	while(1) {
 	    _delay_ms(3000);
-	    
+
 		for (int i = 0; i <= totalChars-1; i++) { // for every encoded char in encodedChars: out to leds
 			out_encrypted_char(encodedAndEncryptedChars[i]);  // access sub array located at [i] from a max of [totalChars=11]
+			_delay_ms(1000);
 		}
-		
+
 	    _delay_ms(10000); // loop
 	}
-	
+
 	return 0;
 }
