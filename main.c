bde255d855f6fba683dc98efb8881164a5f2d845
MoonLordTheSecond <moonlordthesecond@abv.bg>
2026-04-28 21:36:23 +0300

    f0.02_feat: new encrypt(), separarate encode()

--- main.c
+++ main.c
@@ -23,19 +23,16 @@ int encodingValues[totalSymbols][3] = {
     {3, 3, 1}, {3, 3, 2}, {3, 3, 3}
 };
 
-int encodedAndEncryptedChars[totalChars][3];
+int encoded	 [totalChars][3] = {0}; 
+int encrypted[totalChars][3] = {0};
 
-int phi[3][3] = { 
+int keyMatrix[3][3] = { 
 	{1,  2, -1},
 	{1,  1,  0},
 	{2,  4, -1}
 };
 
-int psi[3][3] = {
-	{ 1, -1,  3},
-	{-1,  2, -1},
-	{-2,  2, -5}
-}
+int raisedToPower[3][3] = {0};
 
 //-FUNCTIONS---------------------------------------------------------------------------------
 
@@ -44,11 +41,11 @@ void halt_program(void) {
 		PORTB |= (1 << PB0); //RED LED: HIGH
 		PORTB |= (1 << PB1); //YELLOW LED: HIGH
 		PORTB |= (1 << PB2); //GREEN LED: HIGH
-		_delay_ms(100);
+		_delay_ms(50);
 		PORTB &= ~(1 << PB0);
 		PORTB &= ~(1 << PB1);
 		PORTB &= ~(1 << PB2);
-		_delay_ms(100);
+		_delay_ms(50);
 	}
 }
 
@@ -65,27 +62,89 @@ int check_message_compatability(char message[]) {
     return (counter == totalChars) ? 1 : 0; //return 0 if incompatible chars found
 }
 
-//custom encrypt funciton
-void multiply_matricies(int m, int n, int p, int matrixA[m][n], int matrixB[n][p]) {
+void encode_char(char myChar, int encoded[3]) { //halt when found myChar in symbols [i]
+	for (int i = 0; i <= totalSymbols-1; i++) {
+		if (myChar == symbols[i]) {
+			encoded[0] = encodingValues[i][0];
+			encoded[1] = encodingValues[i][1];
+			encoded[2] = encodingValues[i][2];
+			break; 								
+		}
+	}
+}
+
+void raiseToPower(int keyMatrix[3][3], int power, int raisedToPower[3][3]) {
 	
 }
 
-void out_encrypted_char(int encoded[3]) { 
-	for (int red = 0; red < encoded[0]; red++) { // RED LED HIGH ; LOW
-	  PORTB |= (1 << PB0);  _delay_ms(500);
-	  PORTB &= ~(1 << PB0); _delay_ms(500);
-	}
 
-	for (int yellow = 0; yellow < encoded[1]; yellow++) { // YELLOW LED HIGH ; LOW
-	  PORTB |= (1 << PB1);  _delay_ms(500);
-	  PORTB &= ~(1 << PB1); _delay_ms(500);
+/*
+void encode_and_encrypt_char(char myChar, int out[3]) {
+    //encode
+    int encoded[3];
+	for (int i = 0; i <= totalSymbols-1; i++) {
+		if (myChar == symbols[i]) {
+			encoded[0] = encodingValues[i][0];
+			encoded[1] = encodingValues[i][1];
+			encoded[2] = encodingValues[i][2];
+			break;
+		}
 	}
-
-	for (int green = 0; green < encoded[2]; green++) { // GREEN LED HIGH ; LOW
-	  PORTB |= (1 << PB2);  _delay_ms(500);
-	  PORTB &= ~(1 << PB2); _delay_ms(500);
+	//encrypt
+	int temp[3] = {0, 0, 0}; 
+	for (int row = 0; row <= 2; row++) {
+		for (int col = 0; col <= 2; col++) {
+			temp[row] += encryptionKeyMatrix[row][col] * encoded[col];
+		}
+	}
+	for (int i = 0; i <= 2; i++) {
+		int mod = temp[i] % 3;
+		if (mod < 0) {mod += 3;} // account for negatives
+		out[i] = mod + 1;        // from 0..2 to 1..3
 	}
 }
+*/
+void encrypt_char(int encoded[3], int matrix[3][3], int encrypted[3]) {
+    int product[3] = {0}; 
+    for (int row = 0; row <= 2; row++) {
+        for (int col = 0; col <= 2; col++) {
+            product[row] += matrix[row][col] * encoded[col]; // product of multiplication
+        }
+    }
+	
+	int mod = 0;
+    for (int i = 0; i <= 2; i++) {
+        mod = product[i] % 3; 		   // leftover from product/3 
+        if (mod < 0) { 	  		   
+			mod += 3;			   // if leftover<0    leftover + 3
+		}
+        encrypted[i] = mod + 1;    // add 1			   0..2 -> 1..3
+    }
+}
+
+void out_encrypted_char(int encrypted[3]) { 
+	for (int red = 0; red < encrypted[0]; red++) { // RED LED HIGH ; LOW
+	  PORTB |= (1 << PB0);  _delay_ms(250);
+	  PORTB &= ~(1 << PB0); _delay_ms(250);
+	}
+	
+	_delay_ms(750);
+
+	for (int yellow = 0; yellow < encrypted[1]; yellow++) { // YELLOW LED HIGH ; LOW
+	  PORTB |= (1 << PB1);  _delay_ms(250);
+	  PORTB &= ~(1 << PB1); _delay_ms(250);
+	}
+	
+	_delay_ms(750);
+
+	for (int green = 0; green < encrypted[2]; green++) { // GREEN LED HIGH ; LOW
+	  PORTB |= (1 << PB2);  _delay_ms(250);
+	  PORTB &= ~(1 << PB2); _delay_ms(250);
+	}
+	
+	_delay_ms(750);
+}
+
 
 
 int main(void) {
@@ -96,20 +155,19 @@ int main(void) {
     if(check_message_compatability(message)!=1) {
 		halt_program();
 	}
-
+	
 	for(int i = 0; i <= totalChars-1; i++) {
-		encode_and_encrypt_char(message[i], encodedAndEncryptedChars[i]);
+		encode_char (/* char */message[i], 			  encoded[i]);
+		encrypt_char(/* int* */encoded[i], keyMatrix, encrypted[i]);
 	}
 
-	while(1) {
+	while(1) { 
 	    _delay_ms(3000);
-
 		for (int i = 0; i <= totalChars-1; i++) { // for every encoded char in encodedChars: out to leds
-			out_encrypted_char(encodedAndEncryptedChars[i]);  // access sub array located at [i] from a max of [totalChars=11]
+			out_encrypted_char(encrypted[i]);
 			_delay_ms(1000);
 		}
-
-	    _delay_ms(10000); // loop
+	    _delay_ms(10000);
 	}
 
 	return 0;
